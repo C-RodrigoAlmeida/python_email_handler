5bc3ce44ab9589ebd621b5d2b084adbde06d58fd
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-06-27 19:48:40 -0300

    Added registration views and forms

---
+++ src/user/views/registration.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from user.forms.registration import RegistrationForm
+
+class RegistrationView(TemplateView):
+    template_name = "user/registration.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = RegistrationForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data["username"],
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password"],
+            )
+            user.save()
+        return self.form_invalid(form) if user is None else self.form_valid(form, user)
