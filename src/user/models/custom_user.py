a7ab32b8ae7460af0a6fa60ce1c16ec05e29ec0f
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-01 11:00:52 -0300

    Adjusted to a good enough design to start off

--- src/user/models/custom_user.py
+++ src/user/models/custom_user.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from src.core.validators import alphanumeric
 from src.core.base_model import BaseModel
 
-class CustomUser(BaseModel, AbstractUser): 
+class CustomUser(BaseModel, AbstractUser):
+    username = None 
     employee_id = models.CharField(max_length=4, unique=True, db_index=True, validators=[alphanumeric])
     name = models.CharField(max_length=100, null=False)
+
+    USERNAME_FIELD = "employee_id"
+    REQUIRED_FIELDS = ["employee_id", "password"]
