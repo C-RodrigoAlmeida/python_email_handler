8c1660bdedafdc2182b693b388003788ba71026c
Robbobs <roberta.poltronieri.estudante@petrobras.com.br>
2024-07-25 14:44:23 -0300

    added forms at registrarion.py and styled login.html

--- src/user/forms/login.py
+++ src/user/forms/login.py
@@ -7,13 +7,13 @@ class LoginForm(AuthenticationForm):
         label="Employee ID",
         max_length=4,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800', 'placeholder': 'Employee ID',
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Employee ID',
         }),
     )
 
     password = forms.CharField(
         label="Password",
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control bg-slate-800', 'placeholder': 'Password',
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Password',
         }),
     )
--- src/user/forms/registration.py
+++ src/user/forms/registration.py
@@ -1,7 +1,47 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
+from django import forms
 from src.user.models.custom_user import CustomUser
 
-class RegistrationForm(ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['employee_id','name', 'email', 'password']
+class RegistrationForm(CustomUser):
+    # class Meta:
+    #     model = CustomUser
+    #     fields = ['employee_id','name', 'email', 'password']
+
+    name = forms.CharField(
+        label="Name",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Name',
+        }),
+    )
+
+    last_name = forms.CharField(
+        label="Last Name",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Last Name',
+        }),
+    )
+
+    email = forms.EmailField(
+        label="Email",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Email',
+        }),
+    )
+
+    username = forms.CharField(
+        label="Employee ID",
+        max_length=4,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Employee ID',
+        }),
+    )
+
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Password',
+        }),
+    )
