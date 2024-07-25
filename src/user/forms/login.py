d6549d6a30c7d4a3e9be5f36203c91f960cfa1d1
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-25 12:18:49 -0300

    Adjustments to the registration page

--- src/user/forms/login.py
+++ src/user/forms/login.py
@@ -7,9 +7,9 @@ class LoginForm(AuthenticationForm):
         label="Employee ID",
         max_length=4,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 
+            'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700',
             'style': 'text-transform: uppercase',
-            'placeholder': 'Employee ID',
+            'placeholder': 'Type here',
 
         }),
     )
@@ -17,7 +17,7 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(
         label="Password",
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 
-            'placeholder': 'Password',
+            'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700', 
+            'placeholder': 'Type here',
         }),
     )
--- src/user/forms/registration.py
+++ src/user/forms/registration.py
@@ -1,17 +1,16 @@
-# from django.forms import ModelForm
 from django import forms
 from src.user.models.custom_user import CustomUser
 
-class RegistrationForm(CustomUser):
-    # class Meta:
-    #     model = CustomUser
-    #     fields = ['employee_id','name', 'email', 'password']
+class RegistrationForm(forms.ModelForm):
+    class_inheritance = 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700'
+    placeholders = 'Type here'
 
     name = forms.CharField(
         label="Name",
         max_length=100,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Name',
+            'class': class_inheritance,
+            'placeholder': placeholders,
         }),
     )
 
@@ -19,7 +18,8 @@ class RegistrationForm(CustomUser):
         label="Last Name",
         max_length=100,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Last Name',
+            'class': class_inheritance,
+            'placeholder': placeholders,
         }),
     )
 
@@ -27,7 +27,8 @@ class RegistrationForm(CustomUser):
         label="Email",
         max_length=100,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Email',
+            'class': class_inheritance,
+            'placeholder': placeholders,
         }),
     )
 
@@ -35,13 +36,20 @@ class RegistrationForm(CustomUser):
         label="Employee ID",
         max_length=4,
         widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Employee ID',
+            'class': class_inheritance,
+            'placeholder': placeholders,
+            'style': 'text-transform: uppercase'
         }),
     )
 
     password = forms.CharField(
         label="Password",
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8', 'placeholder': 'Password',
+            'class': class_inheritance,
+            'placeholder': placeholders,
         }),
     )
+
+    class Meta:
+        model = CustomUser
+        fields = ['name', 'last_name', 'email', 'username', 'password']
