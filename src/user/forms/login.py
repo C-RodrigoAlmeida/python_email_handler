b49e8578102eebef1028d2534cfcab542d7426b3
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-25 12:50:00 -0300

    Ive reduced verbosity in forms

--- src/user/forms/login.py
+++ src/user/forms/login.py
@@ -3,21 +3,15 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label="Employee ID",
-        max_length=4,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700',
-            'style': 'text-transform: uppercase',
-            'placeholder': 'Type here',
+    fields = ['username', 'password']
+    widgets = {
+        'username': forms.TextInput(attrs={'style': 'text-transform:uppercase;'}),
+        'password': forms.PasswordInput()
+    }
 
-        }),
-    )
-
-    password = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700', 
-            'placeholder': 'Type here',
-        }),
-    )
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700',
+            })
--- src/user/forms/registration.py
+++ src/user/forms/registration.py
@@ -2,54 +2,17 @@ from django import forms
 from src.user.models.custom_user import CustomUser
 
 class RegistrationForm(forms.ModelForm):
-    class_inheritance = 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700'
-    placeholders = 'Type here'
-
-    name = forms.CharField(
-        label="Name",
-        max_length=100,
-        widget=forms.TextInput(attrs={
-            'class': class_inheritance,
-            'placeholder': placeholders,
-        }),
-    )
-
-    last_name = forms.CharField(
-        label="Last Name",
-        max_length=100,
-        widget=forms.TextInput(attrs={
-            'class': class_inheritance,
-            'placeholder': placeholders,
-        }),
-    )
-
-    email = forms.EmailField(
-        label="Email",
-        max_length=100,
-        widget=forms.TextInput(attrs={
-            'class': class_inheritance,
-            'placeholder': placeholders,
-        }),
-    )
-
-    username = forms.CharField(
-        label="Employee ID",
-        max_length=4,
-        widget=forms.TextInput(attrs={
-            'class': class_inheritance,
-            'placeholder': placeholders,
-            'style': 'text-transform: uppercase'
-        }),
-    )
-
-    password = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(attrs={
-            'class': class_inheritance,
-            'placeholder': placeholders,
-        }),
-    )
-
     class Meta:
         model = CustomUser
-        fields = ['name', 'last_name', 'email', 'username', 'password']
+        fields = ['employee_id', 'name', 'last_name', 'email', 'password']
+        widgets = {
+            'employee_id': forms.TextInput(attrs={'style': 'text-transform:uppercase;'}),
+            'password': forms.PasswordInput()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700',
+            })
