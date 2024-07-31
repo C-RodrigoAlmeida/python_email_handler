715285001b8accb293a2a65fb547fbe8e16140ba
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-31 16:26:22 -0300

    Authentication now works properly

--- src/user/forms/login.py
+++ src/user/forms/login.py
@@ -3,14 +3,15 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class LoginForm(AuthenticationForm):
-    fields = ['employee_id', 'password']
+    fields = ['username', 'password']
     widgets = {
-        'employee_id': forms.TextInput(attrs={'style': 'text-transform:uppercase;'}),
+        'username': forms.TextInput(attrs={'style': 'text-transform:uppercase;'}),
         'password': forms.PasswordInput()
     }
 
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = 'Employee ID'
         for field_name, field in self.fields.items():
             field.widget.attrs.update({
                 'class': 'form-control bg-slate-800 mb-8 border-b-2 border-slate-700',
--- src/user/views/login.py
+++ src/user/views/login.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from src.user.forms.login import LoginForm
 
 class LoginView(LoginView):
     template_name = "login.html"
     form_class = LoginForm
+    success_url = reverse_lazy('user:panel')
