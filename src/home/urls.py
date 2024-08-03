0c2a73c1642ec49d03d7e64e1b3686fc3e7565f8
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-03 19:57:42 -0300

    fixing mistakes on home app

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from src.home.views.home import Home
+from src.home.views.about import About
 
 app_name = "home"
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('about/', Home.as_view(), name='about'),
+    path('about/', About.as_view(), name='about'),
 ]
