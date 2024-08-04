05b92423bff4d368eeef1db3ceb4bee54d0d3777
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-03 21:56:23 -0300

    Continuation of the dark mode theme removal

--- src/inbox/forms/message_template.py
+++ src/inbox/forms/message_template.py
@@ -5,3 +5,10 @@ class MessageTemplateForm(ModelForm):
     class Meta:
         model = MessageTemplate
         fields = ['subject', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super(MessageTemplateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control backdrop-blur bg-gray-300/75 mb-8 border-b-2 border-green-900',
+            })
--- src/inbox/forms/recipient_registration.py
+++ src/inbox/forms/recipient_registration.py
@@ -5,3 +5,10 @@ class RecipientRegistrationForm(ModelForm):
     class Meta:
         model = Recipient
         fields = ['name', 'last_name', 'email', 'employee_id']
+
+    def __init__(self, *args, **kwargs):
+        super(RecipientRegistrationForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control backdrop-blur bg-gray-300/75 mb-8 border-b-2 border-green-900',
+            })
