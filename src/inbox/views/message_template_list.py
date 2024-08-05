e51d22c003c132261d0e38fa16300c31baffea56
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-05 20:04:26 -0300

    view update to make it forbidden to access certain parts of the website without a prior login

--- src/inbox/views/message_template_list.py
+++ src/inbox/views/message_template_list.py
@@ -1,7 +1,23 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from src.inbox.models.message_template import MessageTemplate
+from src.user.models.custom_user import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
-class MessageTemplatesListView(ListView):
+class MessageTemplatesListView(ListView, LoginRequiredMixin):
     model = MessageTemplate
     template_name = "message_template_list.html"
     context_object_name = "templates"
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied
+
+        self.custom_user = get_object_or_404(CustomUser, id=self.request.user.id)
+        return MessageTemplate.objects.filter(message_owner=self.custom_user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['custom_user'] = self.custom_user
+        return context
--- src/inbox/views/recipient_list.py
+++ src/inbox/views/recipient_list.py
@@ -1,7 +1,23 @@
-from src.inbox.models.recipient import Recipient
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+from src.inbox.models.recipient import Recipient
+from src.user.models.custom_user import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
-class RecipientListView(ListView):
+class RecipientListView(LoginRequiredMixin, ListView):
     model = Recipient
     template_name = "recipient_list.html"
     context_object_name = "recipients"
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied
+
+        self.custom_user = get_object_or_404(CustomUser, id=self.request.user.id)
+        return Recipient.objects.filter(contact_owner=self.custom_user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['custom_user'] = self.custom_user
+        return context
