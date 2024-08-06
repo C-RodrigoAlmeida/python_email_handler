3f05f766e7a641d3d0de1e0713098abf37f580a7
Roberta Ferraz <rendash0@gmail.com>
2024-08-06 19:49:05 -0300

    styled recipients list

--- src/inbox/forms/message_template.py
+++ src/inbox/forms/message_template.py
@@ -11,9 +11,9 @@ class MessageTemplateForm(ModelForm):
         super(MessageTemplateForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             if field_name == 'subject':
-                field.widget.attrs.update({'class': 'backdrop-blur bg-gray-300/75 mb-8 border-b-2 border-green-900'})
+                field.widget.attrs.update({'class': 'block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500'})
             else:
-                field.widget.attrs.update({'class': 'backdrop-blur bg-gray-300/75 mb-8 border-2 border-green-900'})
+                field.widget.attrs.update({'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'})
 
     def save(self, commit=True):
         instance = super(MessageTemplateForm, self).save(commit=False)
