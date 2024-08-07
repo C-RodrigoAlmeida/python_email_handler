2cd54a4984c9cfe3882af843c8d61fbcba900033
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-07 10:07:59 -0300

    css adjusments to recipient registration

--- src/inbox/forms/recipient_registration.py
+++ src/inbox/forms/recipient_registration.py
@@ -1,20 +1,23 @@
-from django.forms import ModelForm
+from django import forms
 from src.inbox.models.recipient import Recipient
 
-class RecipientRegistrationForm(ModelForm):
+class RecipientRegistrationForm(forms.ModelForm):
     class Meta:
         model = Recipient
-        fields = ['name', 'last_name', 'email', 'employee_id']
+        fields = ['employee_id', 'name', 'last_name', 'email']
+        widgets = {
+            'employee_id': forms.TextInput(attrs={'style': 'text-transform:uppercase;'}),
+            'password': forms.PasswordInput()
+        }
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
         super(RecipientRegistrationForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs.update({
-                'class': 'form-control backdrop-blur bg-gray-300/75 mb-8 border-b-2 border-green-900',
+                'class': 'form-control backdrop-blur bg-[#202c33] mb-8 border-b-2 border-gray-300 w-full',
             })
-
-
+    
     def save(self, commit=True):
         instance = super(RecipientRegistrationForm, self).save(commit=False)
         instance.contact_owner = self.user
