da74f995e5da8aa29fa90a4d23e765e9b20fcd07
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-06 22:50:16 -0300

    pagination adjustments

--- src/inbox/views/recipient_list.py
+++ src/inbox/views/recipient_list.py
@@ -3,20 +3,16 @@ from django.views.generic import ListView
 from src.inbox.models.recipient import Recipient
 from src.user.models.custom_user import CustomUser
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 
 class RecipientListView(LoginRequiredMixin, ListView):
     model = Recipient
     template_name = "recipient_list.html"
     context_object_name = "recipients"
-    paginate_by = 10
+    paginate_by = 5
 
     def get_queryset(self):
-        if not self.request.user.is_authenticated:
-            raise PermissionDenied
-
         self.custom_user = get_object_or_404(CustomUser, id=self.request.user.id)
-        return Recipient.objects.filter(contact_owner=self.custom_user)
+        return Recipient.objects.filter(contact_owner=self.custom_user).order_by('id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
