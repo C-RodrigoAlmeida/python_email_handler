86f670535b3f12fcffe716e75c479806e149e27b
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-07 16:04:51 -0300

    hover color adjustmnents

--- src/inbox/forms/message_template.py
+++ src/inbox/forms/message_template.py
@@ -13,11 +13,11 @@ class MessageTemplateForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             if field_name == 'subject' or field_name == 'description':
                 field.widget.attrs.update({
-                    'class': 'form-control backdrop-blur bg-[#202c33] mb-8 border-b-2 border-gray-300 w-full focus:outline-none focus:border-emerald-600',
+                    'class': 'form-control backdrop-blur bg-[#202c33] mb-8 border-b-2 border-gray-300 w-full focus:outline-none focus:border-teal-800',
                 })
             else:
                 field.widget.attrs.update({
-                    'class': 'form-control backdrop-blur bg-[#202c33] mb-8 border-2 border-gray-300 w-full focus:outline-none focus:border-emerald-600',
+                    'class': 'form-control backdrop-blur bg-[#202c33] mb-8 border-2 border-gray-300 w-full focus:outline-none focus:border-teal-800',
                 })
     
     def save(self, commit=True):
