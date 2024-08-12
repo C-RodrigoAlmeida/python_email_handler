d08956d447afd5264fea5f3bcf629c70cb7e7340
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-08-12 16:40:49 -0300

    search bar added for recipients list

--- src/inbox/views/recipient_list.py
+++ src/inbox/views/recipient_list.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from src.inbox.models.recipient import Recipient
@@ -18,21 +19,38 @@ class RecipientListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         if not self.request.user.is_authenticated:
             raise PermissionDenied
-        
         self.custom_user = get_object_or_404(CustomUser, id=self.request.user.id)
-        return Recipient.objects.filter(Q(contact_owner=self.custom_user)  & Q(deleted_at__isnull=True)).order_by('id')
+
+        search = self.request.GET.get('search', '')
+        if search:
+            return Recipient.objects.filter(
+                Q(contact_owner=self.custom_user) 
+                & Q(deleted_at__isnull=True) 
+                & (Q(name__icontains=search) 
+                   | Q(last_name__icontains=search) 
+                   | Q(email__icontains=search) 
+                    |Q(employee_id__icontains=search))
+            ).order_by('name')
+        else:
+            return Recipient.objects.filter(Q(contact_owner=self.custom_user) & Q(deleted_at__isnull=True)).order_by('name')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['custom_user'] = self.custom_user
-        paginator = Paginator(self.object_list, self.paginate_by)
-        page = self.request.GET.get('page')
+            context = super().get_context_data(**kwargs)
+            context['custom_user'] = self.custom_user
 
-        try:
-            context['pagination'] = paginator.page(page)
-        except PageNotAnInteger:
-            context['pagination'] = paginator.page(1)
-        except EmptyPage:
-            context['pagination'] = paginator.page(paginator.num_pages)
+            search = self.request.GET.get('search', '')
+            if search:
+                context['search'] = search
 
-        return context
+
+            paginator = Paginator(self.object_list, self.paginate_by)
+            page = self.request.GET.get('page')
+
+            try:
+                context['pagination'] = paginator.page(page)
+            except PageNotAnInteger:
+                context['pagination'] = paginator.page(1)
+            except EmptyPage:
+                context['pagination'] = paginator.page(paginator.num_pages)
+
+            return context
