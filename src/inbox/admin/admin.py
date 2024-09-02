350370cce0791460e05940f04aae24be530435af
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-09-02 00:29:29 -0300

    created tabularinline for recipients in groups

--- src/inbox/admin/admin.py
+++ src/inbox/admin/admin.py
@@ -5,7 +5,23 @@ from src.inbox.models.recipient import Recipient
 from src.inbox.models.message_template import MessageTemplate
 # Register your models here.
 
+class RecipientInline(admin.TabularInline):
+    model = Group.recipients.through
+    extra = 0 
+    can_delete = True
+    verbose_name = "Recipient"
+    verbose_name_plural = "Recipients"
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [RecipientInline]
+    list_display = ('name', 'description', 'group_owner', 'display_recipients')
+
+    def display_recipients(self, obj):
+        return ", ".join([recipient.name for recipient in obj.recipients.all()])
+
+    display_recipients.short_description = 'Recipients'
+
 admin.site.register(SentMessage)
-admin.site.register(Group)
 admin.site.register(Recipient)
 admin.site.register(MessageTemplate)
