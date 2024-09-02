87ddc0ed421589816c47ab39f7aef2808225f375
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-09-02 10:05:42 -0300

    added filters to customuser admin page

--- src/user/admin/admin.py
+++ src/user/admin/admin.py
@@ -5,3 +5,4 @@ from src.user.models.custom_user import CustomUser
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('employee_id', 'first_name', 'last_name', 'email')
     search_fields = ('employee_id', 'first_name', 'last_name', 'email')
+    list_filter = ('is_superuser', 'employee_id', 'first_name', 'last_name', 'email')
