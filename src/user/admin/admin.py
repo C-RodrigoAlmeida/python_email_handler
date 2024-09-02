1b17e92bebad9d2361006cf97dc39c56e0f554ce
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-09-02 10:00:35 -0300

    added a search bar for customuser model

--- src/user/admin/admin.py
+++ src/user/admin/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from src.user.models.custom_user import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('employee_id', 'first_name', 'last_name', 'email')
+    search_fields = ('employee_id', 'first_name', 'last_name', 'email')
