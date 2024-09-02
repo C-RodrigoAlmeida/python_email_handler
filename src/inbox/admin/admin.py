8496c9315fa18cdccc7bfb6618afb5f0c3757044
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-09-02 10:09:09 -0300

    added filters to inbox admin models

--- src/inbox/admin/admin.py
+++ src/inbox/admin/admin.py
@@ -15,8 +15,8 @@ class RecipientInline(admin.TabularInline):
 class GroupAdmin(admin.ModelAdmin):
     inlines = [RecipientInline]
     list_display = ('name', 'description', 'group_owner', 'display_recipients')
-
     search_fields = ('name', 'description')
+    list_filter = ('name', 'description', 'group_owner')
 
     def display_recipients(self, obj):
         return ", ".join([recipient.name for recipient in obj.recipients.all()])
@@ -28,13 +28,16 @@ class GroupAdmin(admin.ModelAdmin):
 class SentMessageAdmin(admin.ModelAdmin):
     list_display = ('sent_by', 'subject', 'message', 'recipients', 'groups')
     search_fields = ('sent_by', 'subject', 'message', 'recipients', 'groups')
+    list_filter = ('sent_by', 'subject', 'message', 'recipients', 'groups')
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
     list_display = ('employee_id', 'name', 'email')
     search_fields = ('employee_id', 'name', 'email')
+    list_filter = ('employee_id', 'name', 'email')
 
 @admin.register(MessageTemplate)
 class MessageTemplateAdmin(admin.ModelAdmin):
     list_display = ('subject', 'description', 'message_owner')
     search_fields = ('subject', 'description')
+    list_filter = ('subject', 'description', 'message_owner')
