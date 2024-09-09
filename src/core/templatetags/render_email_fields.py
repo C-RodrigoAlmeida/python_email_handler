674762273e8bffcb1d6de1f5935b9c2d4dfb3126
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-09-09 08:39:53 -0300

    adding column choice flexibility for users

--- src/core/templatetags/render_email_fields.py
+++ src/core/templatetags/render_email_fields.py
@@ -9,5 +9,7 @@ def render_email_field(request, field_name, recipients, groups):
         'field_name': field_name,
         'recipients': recipients,
         'groups': groups,
-        'value': f'{request.GET.get(field_name, "")}'
+        'value': f'{request.GET.get(field_name, "")}',
+        'field_name_xlsx_column': f'{request.GET.get(field_name + "_xlsx_column", "")}',
+        'columns': [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     }
